69e9dca3b8a7731a685ee051428f3ee86aa44104
Yanagi3256 <tkzm3256@gmail.com>
2024-11-15 09:45:48 +0900

    追記

--- test.c
+++ test.c
@@ -2,7 +2,26 @@
 #include <stdio.h>
 
 int main(){
+    int a,b;
+    a=0;
+    b=0;
+
     printf("Hello World!");
+
+    printf("１つ目の整数を入力してください：");
+    scanf("%d",&a);
+    printf("２つ目の整数を入力してください：");
+    scanf("%d",&b);
+
+    if(a>b){
+        printf("%dは%dより大きい",a,b);
+    }
+    else if(b>a){
+        printf("%dは%dより大きい",b,a);
+    }
+    else{
+        printf("%dと%dは等しい"a,b);
+    }
 
     return 0;
 }
